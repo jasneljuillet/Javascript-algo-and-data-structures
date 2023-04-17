2a2bc64d8ec01bc6f8b18bd9df3f48a0fa38d9ad
jasneljuillet <jasneljuillet1995@gmail.com>
2023-04-16 22:02:59 -0400

    Break down some challenge on freecodecamp

--- challenges-js/ReverseaString.js
+++ challenges-js/ReverseaString.js
@@ -3,10 +3,8 @@ Reverse the provided string and return the reversed string.
 For example, "hello" should become "olleh".
 */
 
-const log = console.log;
-
 function reverseToString(str) {
   return str.split("").reverse().toString().replace(/,/g, "");
 }
 
-log(reverseToString("hello"));
+reverseToString("hello");
