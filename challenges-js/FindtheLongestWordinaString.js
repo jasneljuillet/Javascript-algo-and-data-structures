68d9649bd07d824dc82ad87cb670243107f66633
jasneljuillet <jasneljuillet1995@gmail.com>
2023-04-21 21:06:30 -0400

    bigest word

--- challenges-js/FindtheLongestWordinaString.js
+++ challenges-js/FindtheLongestWordinaString.js
@@ -5,7 +5,15 @@ Your response should be a number.
  */
 
 function findLongestWordLength(str) {
-  return str.length;
+  str = str.split(" ");
+
+  let longest = str[0];
+  for (let i = 0; i < str.length; i++) {
+    if (longest.length < str[i].length) {
+      longest = str[i];
+    }
+  }
+  return longest.length;
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
